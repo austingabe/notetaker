e48a7e8a9fd9bf53054a76d1f0e7be42ee9e35dd
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-06-20 23:04:13 -0700

    Note objects can be deleted from db.json; moved db.json file to Develop directory

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,22 +1,11 @@
-const noteData = require("../db/db.json");
-const express = require("express");
+const noteData = require("../db.json");
+const path = require("path");
 const fs = require("fs");
-const router = express.Router();
 
 module.exports = app => {
   app.get("/api/notes", (req, res) => {
     return res.json(noteData);
   });
-
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // Then the server saves the data to the tableData array)
-
-  //form in the application - get the body info and put it into the form and send it to db.json. then refresh the browser so the db.json will appear with the new note that was added.
-  //fs method that edits an already existing file
-  // fs.appendFileSync('message.txt', 'data to append');
-  //fs.writeFileSync
-  //data capture in variable to pass through to json
 
   app.post("/api/notes", (req, res) => {
     let newNote = req.body;
@@ -27,28 +16,27 @@ module.exports = app => {
     noteData.push(newNote);
     let updateNote = JSON.stringify(noteData);
 
-    fs.writeFileSync("db/db.json", updateNote, err => {
+    fs.writeFileSync(path.basename("/develop/db.json"), updateNote, err => {
       if (err) throw err;
-      console.log("Note Saved");
     });
   });
 
-  app.delete("/api/notes:id", (req, res) => {
-    fs.readFile(__dirname + "db/db.json", utf8, (err, data) => {
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(path.basename("/develop/db.json"), "utf8", (err, data) => {
       if (err) throw err;
-    let allNotes = JSON.parse(noteData);
-    const deleteNote = allNotes.find(notes => notes.id === parseInt(req.params.id));
+      let allNotes = JSON.parse(data);
+      const deleteNote = allNotes.find(notes => notes.id === parseInt(req.params.id));
 
-    const notesIndex = allNotes.indexOf(deleteNote);
+      const notesIndex = allNotes.indexOf(deleteNote);
 
-    allNotes.splice(notesIndex, 1);
+      allNotes.splice(notesIndex, 1);
 
-    fs.writeFileSync(__dirname + "db/db.json", JSON.stringify(allNotes), (err, data) => {
-      if (err) throw err;
-      res.json(allNotes)
+      fs.writeFileSync(path.basename("/develop/db.json"), JSON.stringify(allNotes), (err, data) => {
+        if (err) throw err;
+        res.json(allNotes)
+      });
+
     });
 
-    });
-    
   });
 };
